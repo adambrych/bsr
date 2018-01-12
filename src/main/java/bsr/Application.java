ac306630301930d056811f58c35758f08401734f
Adam <adambrych@gmail.com>
2018-01-12 10:33:46 +0100

    basic auth

--- src/main/java/bsr/Application.java
+++ src/main/java/bsr/Application.java
@@ -3,6 +3,8 @@ package bsr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 870011732533022290709476686866 870011732533022290709476686867
+
 @SpringBootApplication
 public class Application {
 
